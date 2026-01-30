6de32680d0d5772640f85d73a412e988680f8487
ULRIKE KUNZE <ULRIKE.E.KUNZE@GMAIL.COM>
2026-01-30 18:10:15 +0000

    feat:started on profile page

--- src/app/profile/[username]/page.js
+++ src/app/profile/[username]/page.js
@@ -4,7 +4,26 @@
 
 //The Clerk user id does NOT exists until the user signs up - as a new user I should not be able to see user page until I am signed up
 
-export default function ProfilePage() {
+import { db } from "@/utils/dbConnection";
+import Image from "next/image";
+import PostCard from "@/components/Postcard";
+import { notFound } from "next/navigation";
+
+export default async function ProfilePage({ params }) {
+  const { username } = await params;
+
+  const query = await db.query(`SELECT * FROM profiles WHERE username = $1`, [
+    username,
+  ]);
+  console.log(query);
+
+  const data = query.rows[0];
+  console.log(data);
+
+  if (query.rows.length === 0) {
+    notFound();
+  }
+
   //db queries to GET data from the tables
   return (
     <>
